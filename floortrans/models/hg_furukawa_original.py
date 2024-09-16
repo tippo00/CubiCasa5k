6905086f3ab623a60bd99d1a5eefb65c3ed4824c
TimS <88945483+tippo00@users.noreply.github.com>
2024-09-16 20:33:02 +0200

    Add relative import to hg_furukawa_original.py

--- floortrans/models/hg_furukawa_original.py
+++ floortrans/models/hg_furukawa_original.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -233,7 +234,8 @@ class hg_furukawa_original(nn.Module):
         # Pre-trained network weights from Human pose estimation via Convolutional Part Heatmap Regression
         # https://www.adrianbulat.com/human-pose-estimation MPII
         model = model_1427.model_1427
-        model.load_state_dict(torch.load('floortrans/models/model_1427.pth'))
+        ROOT_DIR = Path(__file__).parent
+        model.load_state_dict(torch.load(ROOT_DIR / 'model_1427.pth', weights_only=True))
 
         for (src, dst) in zip(model.parameters(), self.parameters()):
             dst[:].data.copy_(src[:].data)
